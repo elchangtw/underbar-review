a852ef484192a28389c3bc232bf7c8b91877b205
elchangtw <elchang.tw@gmail.com>
2019-10-22 11:27:06 -0700

    completed halfway through part 1; currently on uniq

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,6 +38,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length - 1] : array.slice(Math.max(array.length - n, 0), array.length);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +47,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if ( Array.isArray(collection) ) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else if (typeof collection === 'object') {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +77,55 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item, index, collection) {
+      if ( test(item) ) {
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = _.filter(collection, function(item, index, collection) {
+      if ( !test(item) ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return result;
   };
+
+
+  // Example:
+  // Input: [1, 2, 3, 4, 5, 6]
+  // Iterator: multiplyBy2
+  // TransformedArr: [2, 4, 6, 10, 12]
+  // Result: []
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var result = [];
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+    // create a new array that is the result of running every single item in the original array through iterator
+    var cache = [];
+    _.each(array, function(item) {
+      // if result of this item ran through iterator exists, don't do anything
+      // if result does not exist, save result in cache
+      cache.push( iterator(item) );
+    });
+    _.each(cache, function(item, index) {
+      if ( index === _.indexOf(item) ) {
+        result.push(array[index]);
+      }
+    });
+    return result;
   };
 
 
@@ -107,19 +157,19 @@
   // Reduces an array or object to a single value by repetitively calling
   // iterator(accumulator, item) for each item. accumulator should be
   // the return value of the previous iterator call.
-  //  
+  //
   // You can pass in a starting value for the accumulator as the third argument
   // to reduce. If no starting value is passed, the first element is used as
   // the accumulator, and is never passed to the iterator. In other words, in
   // the case where a starting value is not passed, the iterator is not invoked
   // until the second element, with the first element as its second argument.
-  //  
+  //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //  
+  //
   //   var identity = _.reduce([5], function(total, number){
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
